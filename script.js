ec1a54a37366ef2a995629e4a4f4943130f109a3
poghostick <arsen.v.poghosyan@gmail.com>
2023-04-08 13:32:19 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function getInteger() {
+  const withMs = parseInt(document.getElementById("with-multisport").value);
+  const woMs = parseInt(document.getElementById("without-multisport").value);
+  const feePer30Min = parseInt(document.getElementById("fee-per-30-min").value);
+  const duration = parseInt(document.getElementById("duration").value);
+  const numCourts = parseInt(document.getElementById("num-courts").value);
+
+  let numRounds = Math.round(duration / 30)
+  let totalFee = feePer30Min * Math.max(numRounds - withMs, 0) * (numCourts)
+  let feeWoMs = totalFee / ((numRounds - 1) / numRounds * withMs + woMs)
+  let feeWithMs = (numRounds - 1) / numRounds * feeWoMs
+  
+  document.getElementById("total-fee").value = totalFee;
+  document.getElementById("fee-multisport").value = feeWithMs;
+  document.getElementById("fee-no-multisport").value = feeWoMs;
+}
