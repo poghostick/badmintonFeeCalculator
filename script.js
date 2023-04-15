f1831ee9f44d6f74c21f89e4576d008a36bc357a
poghostick <arsen.v.poghosyan@gmail.com>
2023-04-15 18:55:19 +0200

    dev: Change the logic of fee calculation

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ function getInteger() {
   const numCourts = parseInt(document.getElementById("num-courts").value);
 
   let numRounds = Math.round(duration / 30)
-  let totalFee = feePer30Min * Math.max(numCourts * numRounds - withMs, 0)
-  let feeWoMs = totalFee / ((numRounds - 1) / numRounds * withMs + woMs)
-  let feeWithMs = (numRounds - 1) / numRounds * feeWoMs
+  let totalFeeOrig = feePer30Min * numCourts * numRounds
+  let totalFeeDisc = feePer30Min * Math.max(numCourts * numRounds - withMs, 0)
+  let origFeePerPlayer = totalFeeDisc / (withMs + woMs)
+  let feeWithMulti = Math.max(0, origFeePerPlayer - feePer30Min)
+  let feeWoMulti = (totalFeeOrig - feeWithMulti * withMs) / withMs
   
-  document.getElementById("total-fee").value = totalFee;
-  document.getElementById("fee-multisport").value = feeWithMs;
-  document.getElementById("fee-no-multisport").value = feeWoMs;
+  document.getElementById("total-fee-orig").value = totalFeeOrig;
+  document.getElementById("total-fee-disc").value = totalFeeDisc;
+  document.getElementById("fee-multisport").value = feeWithMulti;
+  document.getElementById("fee-no-multisport").value = feeWoMulti
 }
